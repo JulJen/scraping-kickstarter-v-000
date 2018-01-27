a24edeb8c261d99a952668a5113f95a73b1c6ca0
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-27 21:55:39 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,5 +3,8 @@ require 'nokogiri'
 # require libraries/modules here
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
   # write your code here
 end
